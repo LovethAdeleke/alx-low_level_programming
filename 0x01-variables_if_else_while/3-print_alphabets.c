f5ae6f1d11eabe12ff97ff2afc23a38dd955509b
LovethAdeleke <loveth.adeleke@gmail.com>
2023-04-24 19:43:34 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print alphbets
+ * Return: 0
+ */
+int main(void)
+{
+	char alph;
+	char ALPH;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		putchar(alph);
+	}
+	for (ALPH = 'A'; ALPH <= 'Z'; ALPH++)
+	{
+		putchar(ALPH);
+	}
+	putchar('\n');
+	return (0);
+}
