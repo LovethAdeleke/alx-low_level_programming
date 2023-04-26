a1bcace0cb7af016e931bb398b3a90a4fc5de556
LovethAdeleke <loveth.adeleke@gmail.com>
2023-04-26 12:04:18 -0700

    modified 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,11 @@ void print_alphabet_x10(void)
 	int count = 0;
 	char alph;
 
-	while (count++ <= 9)
+	while (count <= 9)
 	{
 		for (alph = 'a'; alph <= 'z'; alph++)
-			_putchar((alph);
+			_putchar(alph);
 	_putchar('\n');
+	count++;
 	}
 }
