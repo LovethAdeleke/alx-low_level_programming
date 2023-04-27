15e22a1ed7e45839933baee1b2758872975e0655
LovethAdeleke <loveth.adeleke@gmail.com>
2023-04-26 21:50:21 -0700

    Addition

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,15 +3,9 @@
  * add - add two intergers
  * @a: interger 1
  * @b: interger 2
- * sum - result of the addition
  * Return: result
  */
 int add(int a, int b)
 {
-	int sum;
-
-	while (sum = a + b)
-	{
-		return (sum);
-	}
+	return (a+b);
 }
