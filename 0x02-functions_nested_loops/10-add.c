40337ff80e9c49bfafbda600feb4ddf53def24e9
LovethAdeleke <loveth.adeleke@gmail.com>
2023-04-26 21:41:25 -0700

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - add two intergers
+ * @a: interger 1
+ * @b: interger 2
+ * sum - result of the addition
+ * Return: result
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	while (sum = a + b)
+	{
+		return (sum);
+	}
+}
