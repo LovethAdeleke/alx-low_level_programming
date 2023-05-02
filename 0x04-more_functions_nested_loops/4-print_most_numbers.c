fa9c2e30aa490eaaa711b223cbd74001bfdbd3de
LovethAdeleke <loveth.adeleke@gmail.com>
2023-05-01 19:21:03 -0700

    Print numbers 0 to 9, but not 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - print number 0 to 9
+ *  except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int N;
+
+	for (N = 48; N <= 57; N++)
+	{
+		if (N != 50 && N != 52)
+		{
+			_putchar(N);
+		}
+		_putchar('\n');
+	}
+}
