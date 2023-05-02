06568d5c461db21f3fef3aaf866ac46e2fd93c53
LovethAdeleke <loveth.adeleke@gmail.com>
2023-05-01 19:08:21 -0700

    print numbers 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * print_numbers - print numbers 0 to 9
- * Return: 0
  */
 void print_numbers(void)
 {
@@ -12,5 +11,4 @@ void print_numbers(void)
 		_putchar(N);
 	}
 	_putchar('\n');
-	return (0);
 }
