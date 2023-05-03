db504d818e1a86b354ce39113c39ed0e35ab8a2a
LovethAdeleke <loveth.adeleke@gmail.com>
2023-05-02 21:19:38 -0700

    Length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - display the length of string
+ * @s: string, pointer
+ * Return: the length of string
+ */
+int _strlen(char *s)
+{
+	int p = 0;
+
+	while (*(s + p) != 0)
+	{
+		P++;
+	}
+	return (p);
+}
